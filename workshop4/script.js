d66a1e5c2129aa63f7aeb7adf2d7b45ebf88f587
ForSchoolJackson <jmh4687@g.rit.edu>
2025-02-24 19:52:19 -0500

    fixed again

--- workshop4/script.js
+++ workshop4/script.js
@@ -9,12 +9,22 @@ const loadPlaces = () => {
     const scene = document.querySelector("a-scene");
 
     places.forEach(place => {
+
+        //entity
         const entity = document.createElement("a-entity");
 
         entity.setAttribute("gps-entity-place", `latitude: ${place.latitude}; longitude: ${place.longitude}`);
         entity.setAttribute("geometry", "primitive: sphere; radius: 1");
-        entity.setAttribute("material", "color: blue");
+        entity.setAttribute("color", "blue");
 
+        // //create sphere
+        // const sphere = document.createElement("a-sphere");
+        // sphere.setAttribute("color", "blue")
+        // sphere.setAttribute("radius", "1");
+        // sphere.setAttribute("position", "0 1 0");
+        // entity.appendChild(sphere);
+
+        //text
         const text = document.createElement("a-text");
         text.setAttribute("value", place.name);
         text.setAttribute("align", "center");
